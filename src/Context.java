cc99282001153c18f4609967d77237d9b82024e5
Manveer SINGH <singh.manveer93@icloud.com>
2023-06-27 12:53:20 +0200

    🔨 Add Program class and Context interface with set and get methods

---
+++ src/Context.java
@@ -0,0 +1,4 @@
+public interface Context {
+    void setVariable(String name, Integer value);
+    Integer getVariable(String name);
+}
---
+++ src/Program.java
@@ -0,0 +1,5 @@
+import java.util.LinkedList;
+
+public class Program extends LinkedList<Instruction> {
+
+}
